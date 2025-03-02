f978bcdfc8b81fc7e207137f38cbca3f8f844687
ashwakshaik14 <ashwakshaik15@gmail.com>
2025-03-02 23:30:11 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import process from 'node:process';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173,
+    host: "0.0.0.0"
+  },
+});
